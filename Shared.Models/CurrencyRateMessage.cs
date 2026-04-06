13c6f35f4f07cfc818a2422080235a9c87a7caeb
Augustine kudiyirippil <43381411+augustinekudiyirippil@users.noreply.github.com>
2026-04-06 14:48:36 +0100

    Add files via upload

---
+++ Shared.Models/CurrencyRateMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Models
+{
+    public class CurrencyRateMessage
+    {
+        public string Base { get; set; }
+        public string Currency { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
