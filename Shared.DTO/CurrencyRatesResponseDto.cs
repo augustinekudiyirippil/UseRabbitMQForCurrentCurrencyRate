f980d46eeca3dd6898afe13f29687a468c6a5b1b
Augustine kudiyirippil <43381411+augustinekudiyirippil@users.noreply.github.com>
2026-04-06 14:48:08 +0100

    Add files via upload

---
+++ Shared.DTO/CurrencyRatesResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTO
+{
+    public class CurrencyRatesResponseDto
+    {
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public List<CurrencyRateDto> Rates { get; set; }
+    }
+}
---
+++ Shared.DTO/Messages/CurrencyRateMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTO.Messages
+{
+    public class CurrencyRateMessage
+    {
+        public string Base { get; set; }
+        public string Currency { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
